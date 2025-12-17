ebecaa946b863f552b87f0e4443b95c555ef3e02
bala-amypo <bala@amypo.in>
2025-12-17 17:49:34 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/DeviceOwnershipRecord.java
@@ -0,0 +1,46 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class DeviceOwnershipRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String serialNumber;
+
+    @Column(nullable = false)
+    private String ownerEmail;
+
+    public DeviceOwnershipRecord() {
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getSerialNumber() {
+            return serialNumber;
+        }
+
+        public void setSerialNumber(String serialNumber) {
+            this.serialNumber = serialNumber;
+        }
+
+        public String getOwnerEmail() {
+            return ownerEmail;
+        }
+
+        public void setOwnerEmail(String ownerEmail) {
+            this.ownerEmail = ownerEmail;
+        }
+        }
+
+        
