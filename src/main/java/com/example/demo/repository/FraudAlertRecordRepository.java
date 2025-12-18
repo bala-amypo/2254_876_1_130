e4fc2a99c73c19f04bc809bce2bea9f50e65b7cb
bala-amypo <bala@amypo.in>
2025-12-18 18:13:52 +0000

    User commit

--- src/main/java/com/example/demo/repository/FraudAlertRecordRepository.java
+++ src/main/java/com/example/demo/repository/FraudAlertRecordRepository.java
@@ -2,11 +2,18 @@ package com.example.demo.repository;
 
 import com.example.demo.model.FraudAlertRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
-public interface FraudAlertRecordRepository extends JpaRepository<FraudAlertRecord, Long> {
-    Optional<FraudAlertRecord> findBySerialNumber(String serialNumber);
+@Repository
+public interface FraudAlertRepository extends JpaRepository<FraudAlertRecord, Long> {
+
+    // Get all unresolved alerts
+    @Query("SELECT f FROM FraudAlertRecord f WHERE f.resolved = false")
+    List<FraudAlertRecord> findUnresolvedAlerts();
+
+    // Get alerts by claimId
     List<FraudAlertRecord> findByClaimId(Long claimId);
 }
