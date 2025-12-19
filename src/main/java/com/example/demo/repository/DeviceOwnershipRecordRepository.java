3b71a28aa5c2c2fffa9e69271fa2d645553093ab
bala-amypo <bala@amypo.in>
2025-12-19 07:48:17 +0000

    User commit

--- src/main/java/com/example/demo/repository/DeviceOwnershipRecordRepository.java
+++ src/main/java/com/example/demo/repository/DeviceOwnershipRecordRepository.java
@@ -2,9 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.model.DeviceOwnershipRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
+@Repository
 public interface DeviceOwnershipRepository extends JpaRepository<DeviceOwnershipRecord, Long> {
-    Optional<DeviceOwnershipRecord> findBySerialNumber(String serialNumber);
 }
--- src/main/java/com/example/demo/repository/WarrantyClaimRecordRepository.java
+++ src/main/java/com/example/demo/repository/WarrantyClaimRecordRepository.java
@@ -2,9 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.model.WarrantyClaimRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
+@Repository
 public interface WarrantyClaimRepository extends JpaRepository<WarrantyClaimRecord, Long> {
-    Optional<WarrantyClaimRecord> findBySerialNumber(String serialNumber);
 }
