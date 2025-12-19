821ee2aaf1e2bb0e7dd24ae15814d6f677e02268
bala-amypo <bala@amypo.in>
2025-12-19 16:05:30 +0000

    User commit

--- src/main/java/com/example/demo/service/DeviceOwnershipService.java
+++ src/main/java/com/example/demo/service/DeviceOwnershipService.java
@@ -1,16 +1,11 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.DeviceOwnershipRecord;
-
 import java.util.List;
 
 public interface DeviceOwnershipService {
-
     DeviceOwnershipRecord registerDevice(DeviceOwnershipRecord device);
-
     DeviceOwnershipRecord getBySerial(String serialNumber);
-
     List<DeviceOwnershipRecord> getAllDevices();
-
     DeviceOwnershipRecord updateDeviceStatus(Long id, boolean active);
 }
