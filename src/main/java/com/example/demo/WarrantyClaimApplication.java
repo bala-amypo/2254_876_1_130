1cdbf14f9501ff2053740c6c797156dade35b6a7
bala-amypo <bala@amypo.in>
2025-12-21 10:08:56 +0000

    User commit

--- src/main/java/com/example/demo/WarrantyClaimApplication.java
+++ src/main/java/com/example/demo/WarrantyClaimApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class WarrantyClaimApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(WarrantyClaimApplication.class, args);
+        System.out.println("=== Warranty Claim Fraud Detector Application Started ===");
     }
 }
