dce3a8e11bc3ff6d1ca54c7eaf067ecbc7f3e16b
bala-amypo <bala@amypo.in>
2025-12-23 19:42:53 +0000

    User commit

---
+++ src/main/java/com/example/demo/DemoBigTestNGSuiteTest.java
@@ -0,0 +1,4 @@
+import com.example.demo.entity.FraudRuleRecord;
+import com.example.demo.entity.WarrantyClaimRecord;
+import com.example.demo.repository.FraudRuleRecordRepository;
+import com.example.demo.repository.WarrantyClaimRecordRepository;
