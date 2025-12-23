e5d1284baba63abad009401b22880309c042a53c
bala-amypo <bala@amypo.in>
2025-12-23 18:12:52 +0000

    User commit

--- src/main/java/com/example/demo/entity/WarrantyClaimRecord.java
+++ src/main/java/com/example/demo/entity/WarrantyClaimRecord.java
@@ -1,45 +1,16 @@
-package com.example.demo.entity;
+package com.example.demo.model;
 
-import com.example.demo.model.DeviceOwnershipRecord;
-import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class WarrantyClaimRecord {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String claimNumber;
-
-    @ManyToOne
-    @JoinColumn(name = "device_id")
-    private DeviceOwnershipRecord deviceOwnershipRecord;
-
-    private String description;
-    private boolean approved;
-
-    public WarrantyClaimRecord() {}
-
-    public WarrantyClaimRecord(String claimNumber, DeviceOwnershipRecord deviceOwnershipRecord, String description, boolean approved) {
-        this.claimNumber = claimNumber;
-        this.deviceOwnershipRecord = deviceOwnershipRecord;
-        this.description = description;
-        this.approved = approved;
-    }
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getClaimNumber() { return claimNumber; }
-    public void setClaimNumber(String claimNumber) { this.claimNumber = claimNumber; }
-
-    public DeviceOwnershipRecord getDeviceOwnershipRecord() { return deviceOwnershipRecord; }
-    public void setDeviceOwnershipRecord(DeviceOwnershipRecord deviceOwnershipRecord) { this.deviceOwnershipRecord = deviceOwnershipRecord; }
-
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
-
-    public boolean isApproved() { return approved; }
-    public void setApproved(boolean approved) { this.approved = approved; }
+    private String productCode;
+    private boolean fraudulent;
 }
--- src/main/java/com/example/demo/listener/TestResultListener.java
+++ src/main/java/com/example/demo/listener/TestResultListener.java
@@ -1,46 +1,46 @@
-// package com.example.demo.listener;
+package com.example.demo.listener;
 
-// import org.testng.ITestContext;
-// import org.testng.ITestListener;
-// import org.testng.ITestResult;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
 
-// public class TestResultListener implements ITestListener {
+public class TestResultListener implements ITestListener {
 
-//     @Override
-//     public void onTestStart(ITestResult result) {
-//         System.out.println("[TEST START] " + result.getName());
-//     }
+    @Override
+    public void onTestStart(ITestResult result) {
+        System.out.println("[TEST START] " + result.getName());
+    }
 
-//     @Override
-//     public void onTestSuccess(ITestResult result) {
-//         System.out.println("[TEST PASS] " + result.getName());
-//     }
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        System.out.println("[TEST PASS] " + result.getName());
+    }
 
-//     @Override
-//     public void onTestFailure(ITestResult result) {
-//         System.out.println("[TEST FAIL] " + result.getName());
-//         if (result.getThrowable() != null) {
-//             result.getThrowable().printStackTrace();
-//         }
-//     }
+    @Override
+    public void onTestFailure(ITestResult result) {
+        System.out.println("[TEST FAIL] " + result.getName());
+        if (result.getThrowable() != null) {
+            result.getThrowable().printStackTrace();
+        }
+    }
 
-//     @Override
-//     public void onTestSkipped(ITestResult result) {
-//         System.out.println("[TEST SKIPPED] " + result.getName());
-//     }
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        System.out.println("[TEST SKIPPED] " + result.getName());
+    }
 
-//     @Override
-//     public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-//         System.out.println("[TEST FAIL PARTIAL] " + result.getName());
-//     }
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+        System.out.println("[TEST FAIL PARTIAL] " + result.getName());
+    }
 
-//     @Override
-//     public void onStart(ITestContext context) {
-//         System.out.println("[TEST SUITE START] " + context.getName());
-//     }
+    @Override
+    public void onStart(ITestContext context) {
+        System.out.println("[TEST SUITE START] " + context.getName());
+    }
 
-//     @Override
-//     public void onFinish(ITestContext context) {
-//         System.out.println("[TEST SUITE FINISH] " + context.getName());
-//     }
-// }
+    @Override
+    public void onFinish(ITestContext context) {
+        System.out.println("[TEST SUITE FINISH] " + context.getName());
+    }
+}
