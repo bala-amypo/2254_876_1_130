2ce06ee191ee62f7a7a8133b73c9db3ba325cca3
bala-amypo <bala@amypo.in>
2025-12-23 10:17:20 +0000

    User commit

--- src/main/java/com/example/demo/repository/StolenDeviceReportRepository.java
+++ src/main/java/com/example/demo/repository/StolenDeviceReportRepository.java
@@ -1,8 +1,9 @@
+package com.example.demo.repository;
+
 import com.example.demo.model.StolenDeviceReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface StolenDeviceReportRepository extends JpaRepository<StolenDeviceReport, Long> {
-    // Use property navigation: device.serialNumber
     List<StolenDeviceReport> findByDevice_SerialNumber(String serialNumber);
 }
--- src/main/java/com/example/demo/repository/WarrantyClaimRecordRepository.java
+++ src/main/java/com/example/demo/repository/WarrantyClaimRecordRepository.java
@@ -1,9 +1,9 @@
+package com.example.demo.repository;
+
 import com.example.demo.model.WarrantyClaimRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface WarrantyClaimRecordRepository extends JpaRepository<WarrantyClaimRecord, Long> {
-    // Use property navigation: device.serialNumber
     List<WarrantyClaimRecord> findByDevice_SerialNumber(String serialNumber);
 }
-`
