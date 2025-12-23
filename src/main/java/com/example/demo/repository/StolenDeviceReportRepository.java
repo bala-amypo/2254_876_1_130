44b6c5df360f9f9b43d82b62e64dfeea3570561a
bala-amypo <bala@amypo.in>
2025-12-23 17:21:53 +0000

    User commit

--- src/main/java/com/example/demo/repository/StolenDeviceReportRepository.java
+++ src/main/java/com/example/demo/repository/StolenDeviceReportRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface StolenDeviceRepository extends JpaRepository<StolenDeviceReport, Long> {
+public interface StolenDeviceReportRepository extends JpaRepository<StolenDeviceReport, Long> {
     List<StolenDeviceReport> findByDeviceSerialNumber(String serialNumber);
 }
