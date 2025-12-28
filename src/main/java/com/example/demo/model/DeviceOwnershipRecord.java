9238ea2fd73d8a482961d96c53f51a5011d7c5c4
bala-amypo <bala@amypo.in>
2025-12-28 13:08:52 +0000

    User commit

--- src/main/java/com/example/demo/model/DeviceOwnershipRecord.java
+++ src/main/java/com/example/demo/model/DeviceOwnershipRecord.java
@@ -1,6 +1,6 @@
 package com.example.demo.model;
 
-import jakarta.persistence.*;  //
+import jakarta.persistence.*; // time related persistnce and jakarta for  importing lib
 import java.time.LocalDate;
 
 @Entity
